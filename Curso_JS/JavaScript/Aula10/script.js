2655bf25962b8868365bb1f3d7779fd8af19ff1b
Rangerson Clemente <rangerson14@outlook.com>
2023-04-23 18:30:00 -0300

    Var e Let

---
+++ Curso_JS/JavaScript/Aula10/script.js
@@ -0,0 +1,20 @@
+// Tipos de dados
+// *** Dados primitivos ***
+// String
+let nome = 'Vieira'
+let cor = 'vermelho'
+
+// Number - 'Int'
+const idade = 13
+
+//Number - 'Float - Ponto flutuante'
+const preco = 13.78
+
+//Undefined -> Não aponta nenhum local na memoria
+let Complemento; // Variavel apenas declarada, não é inicializada (nulo por padrão)
+
+// Nulo
+let Complemento1 = Null // Variavel declarada nula
+
+// Boolean
+const aprovado = true // Boolean -> true or false (fator logico)
---
+++ Curso_JS/JavaScript/Aula9/script.js
@@ -0,0 +1,9 @@
+// Diferenças entre VAR e LET
+// Uma VAR de variavel pode ser redeclarada varias vezes sem dar erro
+// Uma LET de variavel pode ser declarada apenas uma vez
+
+let meuNome = 'Vieira'
+var MeuNome = 'Rangerson'
+var MeuNome = 'Vieira'
+
+console.log(MeuNome);
