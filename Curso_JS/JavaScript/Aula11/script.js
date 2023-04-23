cc4d95b11d506762383e27159571d73461e6a2bb
Rangerson Clemente <rangerson14@outlook.com>
2023-04-23 18:41:47 -0300

    Create script.js

---
+++ Curso_JS/JavaScript/Aula11/script.js
@@ -0,0 +1,20 @@
+
+//    * Operadores aritméticos
+//    *  + Adiçao ou Comcatenação
+const n1 = 3
+const n2 = 8
+console.log(n1+n2)
+
+console.log(n2+' é um numero e '+n1+' é um outro numero')
+
+//    *  - Substração
+const n3 = 17
+const n4 = 14
+console.log(n4-n3)
+
+//    *  * Multiplicação
+//    *  / Divisão
+//    *  ** Potenciação (Ex.: 4**2 = 16)
+//    *  % Resto de divisão
+
+ 
